95723a09ccdf2cd39e61bdf51e4e70c778c905a7
Aleksandr2905 <natalukha@ukr.net>
2024-10-04 22:43:33 +0300

    fix

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
-// import { Server } from "socket.io";
-// import { createServer } from "http";
-
-// const httpServer = createServer();
-
-// const wsServer = new Server(httpServer, {
-//   cors: {
-//     origin: "*",
-//   },
-// });
-
-// let activeUsers = 0;
-
-// wsServer.on("connection", (socket) => {
-//   activeUsers++;
-//   console.log(`User connected. Active users: ${activeUsers}`);
-
-//   wsServer.emit("activeUsers", activeUsers);
-
-//   socket.on("disconnect", () => {
-//     activeUsers--;
-//     console.log(`User disconnected. Active users: ${activeUsers}`);
-//     wsServer.emit("activeUsers", activeUsers);
-//   });
-// });
-
-// httpServer.listen(4000);
-
 import { Server } from "socket.io";
+import { createServer } from "http";
 
-const SocketHandler = (req, res) => {
-  if (res.socket.server.io) {
-    console.log("Socket is already running");
-  } else {
-    console.log("Socket is initializing");
-    const io = new Server(res.socket.server);
-    res.socket.server.io = io;
+const httpServer = createServer();
 
-    io.on("connection", (socket) => {
-      io.emit("activeUsers", io.engine.clientsCount);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
-      socket.on("disconnect", () => {
-        io.emit("activeUsers", io.engine.clientsCount);
-      });
-    });
-  }
-  res.end();
-};
+let activeUsers = 0;
 
-export default SocketHandler;
+wsServer.on("connection", (socket) => {
+  activeUsers++;
+  console.log(`User connected. Active users: ${activeUsers}`);
+
+  wsServer.emit("activeUsers", activeUsers);
+
+  socket.on("disconnect", () => {
+    activeUsers--;
+    console.log(`User disconnected. Active users: ${activeUsers}`);
+    wsServer.emit("activeUsers", activeUsers);
+  });
+});
+
+httpServer.listen(4000);
+
+// import { Server } from "socket.io";
+
+// const SocketHandler = (req, res) => {
+//   if (res.socket.server.io) {
+//     console.log("Socket is already running");
+//   } else {
+//     console.log("Socket is initializing");
+//     const io = new Server(res.socket.server);
+//     res.socket.server.io = io;
+
+//     io.on("connection", (socket) => {
+//       io.emit("activeUsers", io.engine.clientsCount);
+
+//       socket.on("disconnect", () => {
+//         io.emit("activeUsers", io.engine.clientsCount);
+//       });
+//     });
+//   }
+//   res.end();
+// };
+
+// export default SocketHandler;
